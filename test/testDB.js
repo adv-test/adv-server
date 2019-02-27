1309d41700d0bc295e648eb4b2f9337f0e507634
Rodrigo Salgado <Ruy@localhost.localdomain>
2019-02-26 18:57:13 -0600

    Test suite

---
+++ test/testDB.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+
+describe('Test', () => {
+    
+    describe('quicktest',() => {
+        it('should pass',() => {
+            assert.equal(1,1);
+        });
+    });
+
+});
